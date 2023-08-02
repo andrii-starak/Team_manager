1ed5929e10fa2f185ed377d0356191efb9e66f5e
Andrii Starak <andrii.starak6@gmail.com>
2023-08-02 18:25:05 +0300

    added button logic

--- manager.js
+++ manager.js
@@ -1,12 +1,41 @@
-'use strict'
+'use strict';
 document.addEventListener('DOMContentLoaded', () => {
+  const addPlayerButton = document.querySelector('.add_player');
+  const guestButton = document.querySelector('.add_guest');
+  const playerTable = document.querySelector('.player_table');
+  const inputPlayerName = document.querySelector('#player-name');
+  const users = [];
+  let playerAddedStatus = false;
 
-const playerButton = document.querySelector('.add_player')
+  // Logic
+  //
 
-const addedPlayer = function () {
-    document.querySelector('.player_row').classList.add('activated');
-}
-playerButton.addEventListener('click', () => {
-    addedPlayer()
-})
+  const capitalizer = function (string) {
+    const words = string
+      .split(' ')
+      .map(word => word[0].toUpperCase() + word.slice(1));
+    const capitalizedString = words.join(' ');
+    return capitalizedString;
+  };
+
+  const addedPlayer = function () {
+    let playerName = inputPlayerName.value;
+    if (!playerName) return;
+    else {
+      playerName = capitalizer(playerName);
+      console.log(playerName);
+
+      const createdPlayerRow = document.createElement('p');
+      createdPlayerRow.textContent = `${playerName}`;
+      playerTable.append(createdPlayerRow);
+
+      //   статус чи додавав цей користувач вже гравця
+      playerAddedStatus = !playerAddedStatus;
+      addPlayerButton.disabled = true;
+    }
+  };
+
+  addPlayerButton.addEventListener('click', () => {
+    addedPlayer();
+  });
 });
